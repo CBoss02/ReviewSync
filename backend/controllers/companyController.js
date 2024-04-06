484060a9609074d31cb59b6585f466401b70229d
Molly Pierce <molly@pierceclan.net>
2024-04-06 17:42:40 -0500

    Added getCompanyID, modified getRoles, and added addOrUpdateRoles

--- backend/controllers/companyController.js
+++ backend/controllers/companyController.js
@@ -98,3 +98,70 @@ export const addEmployeeToCompany = async (req, res) => {
         res.status(400).send(error.message);
     }
 }
+
+export const getRoles = async (req, res) => {
+    try {
+        const roles = [];
+        const collection = await db.collection("companies").doc(req.body.companyID).collection("roles");
+        const snapshot = await collection.get();
+        if(snapshot.empty)
+        {
+            res.status(200).send([])
+        }
+        else
+        {
+            snapshot.forEach(doc => {
+                roles.push(doc.data());
+            });
+            res.status(200).send(roles)
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+export const addOrUpdateRoles = async (req, res) => {
+    try {
+        const roles = req.body.roles; //array of jsons
+        let ids = []; //keeps track of the Firestore ids present in the roles array
+        let collection = db.collection('companies').doc(req.body.companyID).collection('roles');
+        const snapshot = await collection.get();
+        for(let i = 0; i < roles.length; i++)
+        {
+            ids.push(roles[i].id)
+            for(let j = i + 1; j < roles.length; j++)
+            {
+                if(roles[i].name === roles[j].name)
+                {
+                    res.status(400).send({message: "Role names must be unique"})
+                    return
+                }
+            }
+        }
+        await snapshot.forEach(doc => {
+            if(!ids.includes(doc.id))
+            {
+                collection.doc(doc.id).delete()
+            }
+        })
+        for(let i = 0; i < roles.length; i++) {
+            if (roles[i].id.length < 20) //Adding new role
+            {
+                await collection.add({
+                    name: roles[i].name,
+                    permissions: roles[i].permissions
+                })
+            }
+            else //Updating existing role
+            {
+                await collection.doc(roles[i].id).update({
+                    name: roles[i].name,
+                    permissions: roles[i].permissions
+                })
+            }
+        }
+        res.status(200).send();
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {createCompany, getCompanyID, addEmployeeToPendingList, addEmployeeToCompany} from "../controllers/companyController.js";
+import {createCompany, getCompanyID, addEmployeeToPendingList, addEmployeeToCompany, getRoles, addOrUpdateRoles} from "../controllers/companyController.js";
 const router = express.Router();
 
 router.post("/createCompany", createCompany);
 router.get("/getCompanyID", getCompanyID);
 router.post("/addEmployeeToPendingList", addEmployeeToPendingList);
 router.put("/addEmployeeToCompany", addEmployeeToCompany);
+router.get("/getRoles", getRoles);
+router.put("/addOrUpdateRoles", addOrUpdateRoles);
 
 export default router;
