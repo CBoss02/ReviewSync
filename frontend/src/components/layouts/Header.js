ef8b94b7739102edcaec9e041f546faa30a07e9d
Caleb Boss <ninjatechdude@gmail.com>
2024-04-20 14:43:01 -0500

    Added the company button, additionally conditionally renders the home screen so users can only add or join a company if they do not already have one.

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -13,10 +13,23 @@ import axios from "axios";
 
 export default function Header() {
     const [modal, setModal] = useState(false);
-
     const { currentUser } = useAuth();
 
+    const [companyName, setCompanyName] = useState("");
+
     useEffect(() => {
+
+        //From Edit-roles
+        const fetchCompanyName = async (user) => {
+            try {
+                const response = await axios.post('/api/companies/getCompanyName', {uid: user.uid});
+                setCompanyName(response.data.companyName)
+            } catch (error) {
+                console.error('Failed to fetch company name:', error);
+                // Handle error (e.g., show an error message to the user)
+            }//end try catch
+        };//end fetchCompanyName
+
         const fetchData = async () => {
             try {
                 const user = auth.currentUser;
@@ -28,6 +41,9 @@ export default function Header() {
                         Authorization: `Bearer ${token}`,
                     },
                 };
+
+                fetchCompanyName(user);
+
                 await axios.get("", payloadHeader);
             } catch (e) {
                 console.log(e);
@@ -42,13 +58,24 @@ export default function Header() {
         <>
             <nav className="px- px-2 sm:px-4 py-2.5 bg-gray-50 border-gray-200 dark:bg-gray-800 dark:border-gray-700 text-gray-900 text-sm rounded border dark:text-white">
                 <div className="container mx-auto flex flex-wrap items-center justify-between">
-                    <Link to="/profile" className="flex">
+                    <Link to="/dashboard" className="flex">
             <span className="flex self-center text-lg font-semibold whitespace-nowrap text-gray-900 dark:text-white">
                 <h1 className="text-2xl font-bold text-gray-700 dark:text-gray-200">Review</h1>
               <img src={logo} alt="ReviewSync" className="h-8" />
             </span>
                     </Link>
-                    <div className="flex md:order-2">
+                    <div className="flex md:order-2 ">
+
+                        {/*Code to create the company button in the header*/}
+                        {companyName && currentUser && (
+                            <Link
+                                to="/edit-roles"
+                                className="text-gray-500 dark:text-gray-400 focus:outline-none hover:bg-gray-100 dark:hover:bg-gray-700 rounded-full p-2.5 font-bold my-auto text-lg"
+                            >
+                                {companyName}
+                            </Link>
+                        )}
+
                         <ThemeToggler />
 
                         {currentUser && (
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,7 +4,7 @@ import {useAuth} from "../contexts/AuthContext";
 import axios from 'axios';
 
 //Caleb's code from index.js used to navigate into the /edit-roles page
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import {Link, useNavigate} from "react-router-dom"; // Import useNavigate
 //import auth from "../config/firebase";
 
 export default function Home() {
@@ -26,6 +26,21 @@ export default function Home() {
             setIsVisible(false); // Hide input field
         }
     }, [state]);
+
+    //Gets the user's company if they have one when the page loads
+    useEffect(() => {
+        const fetchCompanyName = async () => {
+            try {
+                const response = await axios.post('/api/companies/getCompanyName', {uid: uid});
+                setCompanyName(response.data.companyName)
+            } catch (error) {
+                console.error('Failed to fetch company name:', error);
+                // Handle error (e.g., show an error message to the user)
+            }//end try catch
+        };//end fetchCompanyName
+
+        fetchCompanyName();
+    }, []);
 
     useEffect(() => {
         const handleClickOutside = (event) => {
@@ -69,42 +84,53 @@ export default function Home() {
     const transitionClass = isVisible ? "opacity-100 scale-100" : "opacity-0 scale-95";
 
     return (
+
         <div className="flex flex-col items-center justify-center h-screen transition-all duration-500">
-            <div className="text-center space-y-3 w-1/4">
-                <button
-                    className={`bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded w-full transition-all duration-500 ${state === 0 ? "block" : "hidden"}`}
-                    onClick={() => setState(1)}
-                >
-                    Join a Company
-                </button>
-                {state !== 0 && (
-                    <div id="inputContainer" className={`flex items-center space-x-5 transition-all duration-500 ${transitionClass}`}>
-                        <input
-                            type="text"
-                            placeholder={state === 1 ? "Search Company" : "Enter New Company Name"}
-                            className="w-full border-2 border-gray-300 p-2 rounded-full transition-all duration-500"
-                            onChange={(e) => setCompanyName(e.target.value)}
-                        />
-                        <button
-                            className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full"
-                            onClick={handleArrowClick}
-                        >
-                            <ArrowRightIcon className="h-6 w-6" />
-                        </button>
-                    </div>
-                )}
-                {state === 0 && (
-                    <p className="text-gray-500 text-sm">
-                        or
-                    </p>
-                )}
-                <button
-                    className={`bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded w-full transition-all duration-500 ${state === 0 ? "block" : "hidden"}`}
-                    onClick={() => { setState(2); }}
-                >
-                    Create a Company
-                </button>
-            </div>
+            {/*If the user doesn't already have a company, allow them to join or create a company*/}
+            {!companyName ? (
+                <div className="text-center space-y-3 w-1/4">
+                    <button
+                        className={`bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded w-full transition-all duration-500 ${state === 0 ? "block" : "hidden"}`}
+                        onClick={() => setState(1)}
+                    >
+                        Join a Company
+                    </button>
+                    {state !== 0 && (
+                        <div id="inputContainer"
+                             className={`flex items-center space-x-5 transition-all duration-500 ${transitionClass}`}>
+                            <input
+                                type="text"
+                                placeholder={state === 1 ? "Search Company" : "Enter New Company Name"}
+                                className="w-full border-2 border-gray-300 p-2 rounded-full transition-all duration-500"
+                                onChange={(e) => setCompanyName(e.target.value)}
+                            />
+                            <button
+                                className="bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full"
+                                onClick={handleArrowClick}
+                            >
+                                <ArrowRightIcon className="h-6 w-6"/>
+                            </button>
+                        </div>
+                    )}
+                    {state === 0 && (
+                        <p className="text-gray-500 text-sm">
+                            or
+                        </p>
+                    )}
+                    <button
+                        className={`bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded w-full transition-all duration-500 ${state === 0 ? "block" : "hidden"}`}
+                        onClick={() => {
+                            setState(2);
+                        }}
+                    >
+                        Create a Company
+                    </button>
+                </div>
+            ) : (
+                //When the user already has a company navigate to the dashboard
+                navigate("/dashboard")
+            )}
+
         </div>
     );
 }
